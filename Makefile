af7208eac28eb6aa0a5b3d1c7188b17c1237349a
luye <luye19980902@163.com>
2021-12-04 20:04:43 -0500

    [FEATURE] add batch test case

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 APP_NAME = correctness_test performance_test
 CORRECTNESS_OBJS = fine_grain/fine_grain.o coarse_grain/coarse_grain.o lock_free/lockfree_bst.o test/correctness_test.o 
-PERFORMANCE_OBJS = fine_grain/fine_grain.o coarse_grain/coarse_grain.o lock_free/lockfree_bst.o test/performance_test.o
-CXX = g++-10 -m64 -std=c++11
+PERFORMANCE_OBJS = fine_grain/fine_grain.o coarse_grain/coarse_grain.o lock_free/lockfree_bst.o test/performance_test_ye.o
+CXX = g++ -m64 -std=c++11
 CXXFLAGS = -I. -O3 -Wall -pthread
 
 default: $(APP_NAME)
@@ -25,4 +25,4 @@ lock_free/%.o: lock_free/%.cpp
 	$(CXX) $< $(CXXFLAGS) -c -o $@
 
 clean:
-	/bin/rm -rf fine_grain/*.o lock_free/*.o test/*.o $(APP_NAME)
+	/bin/rm -rf fine_grain/*.o coarse_grain/*.o lock_free/*.o test/*.o $(APP_NAME)
---
+++ test/performance_test_ye.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+#include "fine_grain/fine_grain.h"
+#include "lock_free/lockfree_bst.h"
+#include "coarse_grain/coarse_grain.h"
+using namespace std;
+using namespace std::chrono;
+
+#define IS_BST 2
+
+FGBST *fgt;
+BST *bst;
+CGBST *cgbst;
+
+void test_init_tree() {
+    if (IS_BST == 1) bst = new BST();
+	else if (IS_BST == 2) cgbst = new CGBST();
+    else fgt = new FGBST();
+}
+
+void test_clear_tree() {
+    if (IS_BST == 1) delete bst;
+	else if (IS_BST == 2) delete cgbst;
+    else delete fgt;
+}
+
+bool test_search_tree(int k) {
+    if (IS_BST == 1) return bst->contains(k);
+	else if (IS_BST == 2) return cgbst->contains(k);
+    else return fgt->contains(k);
+}
+
+bool test_insert_tree(int k) {
+    if (IS_BST == 1) return bst->add(k);
+	else if (IS_BST == 2) return cgbst->add(k);
+    else return fgt->add(k);
+}
+
+bool test_delete_tree(int k) {
+    if (IS_BST == 1) return bst->remove(k);
+	else if (IS_BST == 2) return cgbst->remove(k);
+    else return fgt->remove(k);
+}
+
+void insertRange(int low, int high) {
+    for (int i = low; i < high; i++) {
+        test_insert_tree(i);
+    }
+}
+
+void deleteRange(int low, int high) {
+    for (int i = low; i < high; i++) {
+        test_delete_tree(i);
+    }
+}
+
+void insertDeleteRange(int low, int high) {
+    insertRange(low, high);
+    deleteRange(low, high);
+}
+
+void insertDeleteBatch(int low, int high, int batchSize) {
+    int numBatch = (high - low) / batchSize;
+    if ((high - low) % batchSize != 0) numBatch += 1;
+
+    for (int i = 0; i < numBatch; i++) {
+        int endIdx = low + (i + 1) * batchSize;
+        if (endIdx > high) {
+            endIdx = high;
+        }
+        insertRange(low + i * batchSize, endIdx);
+        deleteRange(low + i * batchSize, endIdx);
+    }
+}
+
+void testInsertDeleteBatch(int numThreads, int threadCapacity, int batchSize) {
+    vector<thread> tvec;
+    for (int i = 0; i < numThreads; i++) {
+        tvec.push_back(thread(insertDeleteBatch, i * threadCapacity, (i+1) * threadCapacity, batchSize));
+    }
+    for (int i = 0; i < numThreads; i++) {
+        tvec[i].join();
+    }
+}
+
+void testInsertDeleteRange(int numThreads, int threadCapacity) {
+    vector<thread> tvec;
+    for (int i = 0; i < numThreads; i++) {
+        tvec.push_back(thread(insertDeleteRange, i * threadCapacity, (i+1) * threadCapacity));
+    }
+    for (int i = 0; i < numThreads; i++) {
+        tvec[i].join();
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int capacity = 10000;
+    vector<int> numThreads = {1, 4, 16, 64, 128};
+    for (int numThread: numThreads) {
+        test_init_tree();
+        auto start = high_resolution_clock::now();
+        testInsertDeleteRange(numThread, capacity);
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<milliseconds>(stop - start);
+        printf("InsertDeleteRange for %d capacity and %d threads: %ld milliseconds\n", capacity, numThread, duration.count());
+        test_clear_tree();
+    }
+    for (int numThread: numThreads) {
+        test_init_tree();
+        auto start = high_resolution_clock::now();
+        testInsertDeleteBatch(numThread, capacity, 100);
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<milliseconds>(stop - start);
+        printf("InsertDeleteBatch for %d capacity and %d threads: %ld milliseconds\n", capacity, numThread, duration.count());
+        test_clear_tree();
+    }
+    return 0;
+}
